9b5318f13a1e1acfc1eb44cd041c432adb251de2
Katy Morgan <kmorgan@gcp-rs-ana5.virago.internal.dtlr.gov.uk>
2024-09-27 12:55:09 +0000

    Made maps

--- Map of our cps and atcs in London.R
+++ Map of our cps and atcs in London.R
@@ -6,16 +6,22 @@ library(leaflegend)
 library(sf)
 library(htmltools)
 
-ATCs <- read.csv('ATCs in London.csv')
+ATCs <- read.csv('ATCs in London.csv') %>% arrange(Site_Number)
 
-CPs <- read.csv('Minor CPs in London.csv')
+CPs <- read.csv('Minor CPs in London.csv') %>% arrange(CP)
 
 ATCs_sf <- st_as_sf(ATCs, coords = c(x = "LONG", y = "LAT"), crs =  "WGS84")
 CPs_sf <- st_as_sf(CPs, coords = c(x = "CP_Longitude", y = "CP_Latitude"), crs =  "WGS84")
 
-ATCs_sf <- ATCs_sf %>% mutate(summary = paste0("<b>", "Site number = ", as.character(Site_Number),"</br> ", "RoadName = " , ROADNAME))
+ATCs_sf <- ATCs_sf %>% mutate(summary = paste0("<b>", "Site number = ", as.character(Site_Number),
+                                               "</br> ", "RoadName = " , ROADNAME,
+                                               "</br>", "RoadNumber = ", Road_Num))
+                                               
 
-CPs_sf <- CPs_sf %>% mutate(summary = paste0("<b>", "CP number = ", as.character(CP),"</br> ", "RoadName = " , Road_Name))
+CPs_sf <- CPs_sf %>% mutate(summary = paste0("<b>", "CP number = ", as.character(CP),
+                                             "</br> ", "RoadName = " , Road_Name,
+                                             "</br>", "RoadNumber = ", Road_Number))
+                                             
                                                                 
 ATCs_B_roads <- ATCs_sf %>% filter(ROADCLASS == 'B')
 ATCs_CU_roads <- ATCs_sf %>% filter(ROADCLASS == 'C' | ROADCLASS == 'U')
@@ -24,46 +30,37 @@ CPs_CU_roads <- CPs_sf %>% filter(Road_Number == 'C' | Road_Number == 'U')
 
 ATC_B_labels <- ATCs_B_roads$summary  %>% lapply(HTML)  
 ATC_CU_labels <- ATCs_CU_roads$summary  %>% lapply(HTML)  
-CP_B_labels <- ATCs_B_roads$summary  %>% lapply(HTML)  
-CP_CU_labels <- ATCs_CU_roads$summary  %>% lapply(HTML)  
+CP_B_labels <- CP_B_roads$summary  %>% lapply(HTML)  
+CP_CU_labels <- CP_CU_roads$summary  %>% lapply(HTML)  
 
                                             
-
-
 leaflet() %>% addProviderTiles(providers$OpenStreetMap) %>%   
-  addCircles(data = ATCs_sf, 
+  addCircles(data = ATCs_B_roads, 
              weight = 5, 
              opacity = 0.8,
              color = 'blue',
-          #   color =  ~binpal(ATC_sites_shape$Strata),#"black",
              fillOpacity = 0.5,
-           #  fillColor =~ binpal(ATC_sites_shape$Strata),# "blue",
              stroke = TRUE,
              radius = 50, 
-            # label = ~labels_2,  
-             #popup = ~labels_1, 
+             label = ~ATC_B_labels,  
+             popup = ~ATC_B_labels, 
              group = "something",
-             
              highlightOptions = highlightOptions(
                weight = 5,
                color = 'blue',
                opacity = 0.6, 
                fillOpacity = 0.8, 
                fillColor = "white")) %>% 
-  
-  addCircles(data = CPs_sf, 
+  addCircles(data = CPs_B_roads, 
              weight = 5, 
              opacity = 0.8,
              color = 'red',
-             #   color =  ~binpal(ATC_sites_shape$Strata),#"black",
              fillOpacity = 0.5,
-             #  fillColor =~ binpal(ATC_sites_shape$Strata),# "blue",
              stroke = TRUE,
              radius = 50, 
-             # label = ~labels_2,  
-             #popup = ~labels_1, 
+             label = ~CP_B_labels ,  
+             popup = ~CP_B_labels, 
              group = "something",
-             
              highlightOptions = highlightOptions(
                weight = 5,
                color = 'red',
@@ -75,15 +72,15 @@ leaflet() %>% addProviderTiles(providers$OpenStreetMap) %>%
 
 
 
-%>% 
-  
-  addSearchFeatures(targetGroups = "something",
-                    options = 
-                      searchFeaturesOptions(
-                        zoom = 12, 
-                        openPopup = TRUE, 
-                        firstTipSubmit = TRUE,
-                        autoCollapse = TRUE, 
-                        hideMarkerOnCollapse = TRUE, 
-                        position =  "topright"
-                      ))
+# 
+#   
+#   addSearchFeatures(targetGroups = "something",
+#                     options = 
+#                       searchFeaturesOptions(
+#                         zoom = 12, 
+#                         openPopup = TRUE, 
+#                         firstTipSubmit = TRUE,
+#                         autoCollapse = TRUE, 
+#                         hideMarkerOnCollapse = TRUE, 
+#                         position =  "topright"
+#                       ))
